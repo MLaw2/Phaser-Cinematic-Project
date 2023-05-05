42ef004f057917e3df8e7a45810f45c644a3bf76
Michael <xxtwomadxx@gmail.com>
2023-05-05 12:54:31 -0700

    added script.js working on scenes now

---
+++ script.js
@@ -0,0 +1,24 @@
+class Intro extends Phaser.Scene{
+    constructor(){
+        super("Intro");
+    }
+    create(){
+        this.graphics = this.add.graphics();
+        this.graphics.fillStyle(0xff9900, 1);
+        this.graphics.fillCircle(100, 100, 50);
+    }
+}
+class GetIn extends Phaser.Scene{
+
+}
+class Startup extends Phaser.Scene{
+
+}
+let config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 800,
+    backgroundColor: 0x000000,
+    scene: [Intro]
+}
+let game = new Phaser.Game(config);
