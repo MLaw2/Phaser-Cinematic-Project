f7523cb44f2099c86e27956f221b3230136f91f3
Michael <xxtwomadxx@gmail.com>
2023-05-05 16:26:29 -0700

    adding more assets, font, and working on scenes

--- script.js
+++ script.js
@@ -2,29 +2,73 @@ class Intro extends Phaser.Scene{
     constructor(){
         super("Intro");
     }
+    preload(){
+    }
     create(){
-        this.graphics = this.add.graphics();
-        this.graphics.fillStyle(0xff9900, 1);
-        this.graphics.fillCircle(100, 100, 50);
+        let logoX = 290;
+        let logoY = 200;
+        let logoSize = 40;
+        let logoSpacing = 50;
+        let logo1 = this.add.text(logoX, logoY, 'I', {
+            fontFamily: 'Teoran',
+            fontSize: logoSize 
+        });
+        let logo2 = this.add.text(logoX, logoY+logoSpacing, 'Like', {
+            fontFamily: 'Teoran',
+            fontSize: logoSize 
+        });
+        let logo3 = this.add.text(logoX, logoY+ (logoSpacing*2), 'Cars', {
+            fontFamily: 'Teoran',
+            fontSize: logoSize
+        });
+        // this.tweens.add()
+        this.time.delayedCall(3000, () =>{
+            this.scene.transition({
+                target: "OpenDoor",
+                duration: 500,
+                onUpdate: this.transitionOut,
+            })
+        })
+    }
+    transitionOut(){
+        ;
     }
 }
-class OpenDoor extends Phaser.scene{
+class OpenDoor extends Phaser.Scene{
     constructor(){
         super("OpenDoor");
     }
     create(){
         this.graphics = this.add.graphics();
+        this.graphics.fillStyle(0xffffff, 1);
+        this.graphics.fillRect(0, 0, 100, 100);
     }
 }
 class GetIn extends Phaser.Scene{
+    constructor(){
+        super("GetIn");
+    }
 }
 class Startup extends Phaser.Scene{
+    constructor(){
+        super("Startup");
+    }
+}
+class MoveOut extends Phaser.Scene{
+    constructor(){
+        super("MoveOut");
+    }
+}
+class MainMenu extends Phaser.Scene{
+    constructor(){
+        super("MainMenu");
+    }
 }
 let config = {
     type: Phaser.WEBGL,
     width: 800,
-    height: 800,
+    height: 600,
     backgroundColor: 0x000000,
-    scene: [Intro]
+    scene: [Intro, OpenDoor, GetIn, Startup, MoveOut, MainMenu]
 }
 let game = new Phaser.Game(config);
